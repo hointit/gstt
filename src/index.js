cd0a047937ed220390bbb0bc8ffd3b373ee363cd
hontit <hointit@gmail.com>
2018-08-03 15:56:08 +0700

    no message

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import 'gsap'
 import './js/custom'
 
 // css
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.css'
+
 import './scss/main.scss'
 import 'owl.carousel'
 import './styles/main_styles.css'
