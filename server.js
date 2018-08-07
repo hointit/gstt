856568aad15809abeb03e4ecd442d8d6c1a3be6c
hontit <hointit@gmail.com>
2018-08-07 14:04:13 +0700

    no message

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-var webpack = require('webpack')
-const path = require("path")
-var WebpackDevServer = require('webpack-dev-server')
-var config = require('./webpack.config')
-var port = process.env.PORT || 6969
+// var webpack = require('webpack')
+// const path = require("path")
+// var WebpackDevServer = require('webpack-dev-server')
+// var config = require('./webpack.config')
+// var port = process.env.PORT || 6969
+//
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   contentBase: path.join(__dirname, 'public'),
+//   hot: true,
+//   inline: true,
+//   historyApiFallback: true,
+// }).listen(port, function (err, result) {
+//   if (err) {
+//     console.log(err)
+//   }
+//   console.log('server started')
+// })
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  contentBase: path.join(__dirname, 'public'),
-  hot: true,
-  inline: true,
-  historyApiFallback: true,
-  open: true
-}).listen(port, function (err, result) {
-  if (err) {
-    console.log(err)
-  }
-  console.log('server started')
-})
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
+
+console.log("server started");
